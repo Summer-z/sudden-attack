3c24119441e3e45e72827d7b8834b82fe4cabdec
Summer-z <979971754@qq.com>
2017-08-08 14:58:32 +0800

    modified1

--- task1.js
+++ task1.js
@@ -1,16 +1,17 @@
-
+/*日期：2017/08/08
+ 题目：将一个正整数分解质因数。例如：输入90,打印出90=2335。*/
 function main(input) {
     let temp = [];
     let start = input;
-    for(let i = 2; i<=input; i++) {
-        while(input%i===0) {
+    for (let i = 2; i <= input; i++) {
+        while (input % i === 0) {
             temp.push(i);
-            input = input/i;
+            input = input / i;
         }
 
     }
     let result;
     result = start + '=' + temp.join('');
-    return result ;
+    return result;
 }
 console.log(main(90));
