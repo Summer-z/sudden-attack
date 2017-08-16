b34e8920ee876341d70df0b020d34f878e64429c
Summer-z <979971754@qq.com>
2017-08-16 14:29:10 +0800

    task0816

---
+++ task0816.js
@@ -0,0 +1,19 @@
+/*日期：2017/08/16
+ 题目：找出数组 arr 中重复出现过的元素
+ 例如：arr = [‘a’, ‘ab’, ‘ed’, ‘da’, ‘a’, ‘ed’, ‘b’];输出：‘a’, ‘ed’
+ */
+let arr = ['a','ab','ed','da','a','ed','b'];
+function findArr(arr) {
+    let temp =[];
+    let result = [];
+    arr.forEach(ele => {
+        if(temp.includes(ele)) {
+            result.push(ele);
+        } else {
+            temp.push(ele);
+        }
+    });
+    return result;
+}
+
+console.log('数组[' + arr + ']中重复出现的元素是：' + findArr(arr));
